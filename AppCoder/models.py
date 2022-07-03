039e92335b965051fa3935e267017380bff9aecb
dg38932 <dg38932@hotmail.com>
2022-07-03 12:42:54 -0300

    commit #7

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -16,6 +16,10 @@ class Profesor(models.Model):
     apellido=models.CharField(max_length=30)
     email=models.EmailField()
     profesion=models.CharField(max_length=30)
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Apellido: {self.apellido} - E-Mail: {self.email} - Profesion: {self.profesion}"
+
+
 
 class Entregable(models.Model):
     nombre=models.CharField(max_length=30)
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 from AppCoder.forms import CursoFormulario
 from AppCoder.forms import ProfesorFormulario
 from AppCoder.models import Curso, Profesor
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 
 # Create your views here.
 
@@ -72,3 +77,56 @@ def buscar(request):
     else:
         respuesta = "No enviaste datos"
     return HttpResponse(respuesta)
+
+def leerProfesores(request):
+    profesores = Profesor.objects.all()
+    contexto= {"profesores":profesores}
+    return render(request, "AppCoder/leerProfesores.html",contexto)
+
+def eliminarProfesor(request, profesor_nombre):
+    profesor = Profesor.objects.get(nombre=profesor_nombre)
+    profesor.delete()
+    profesores = Profesor.objects.all()
+    contexto= {"profesores":profesores}
+    return render(request, "AppCoder/leerProfesores.html",contexto)
+
+def editarProfesor(request, profesor_nombre):
+    profesor = Profesor.objects.get(nombre=profesor_nombre)
+    if request.method == 'POST':
+        miFormulario = ProfesorFormulario(request.POST)
+        print(miFormulario)
+        if miFormulario.is_valid:
+            informacion = miFormulario.cleaned_data
+
+            profesor.nombre = informacion['nombre']
+            profesor.apellido = informacion['apellido']
+            profesor.email = informacion['email']
+            profesor.profesion = informacion['profesion']
+
+            profesor.save()
+            return render(request, "AppCoder/inicio.html")
+    else:
+        miFormulario = ProfesorFormulario(initial={'nombre': profesor.nombre, 'apellido': profesor.apellido, 'email': profesor.email, 'profesion': profesor.profesion})
+    return render(request, "AppCoder/profesorFormulario.html", {"miFormulario":miFormulario, "profesor_nombre": profesor_nombre})
+
+class CursoList(ListView):
+    model = Curso
+    template_name = "AppCoder/cursos_list.html"
+
+class CursoDetalle(DetailView):
+    model = Curso
+    template_name = "AppCoder/cursos_detalle.html"
+
+class CursoCreacion(CreateView):
+    model = Curso
+    success_url = "/AppCoder/curso/list" 
+    fields = ['nombre', 'camada']
+
+class CursoUpdate(UpdateView):
+    model = Curso
+    success_url = "/AppCoder/curso/list"
+    fields = ['nombre', 'camada']
+
+class CursoDelete(DeleteView):
+    model = Curso
+    success_url = "/AppCoder/curso/list"
